1763cd8c21de2fa67059de3cea81694a3fea708f
AIrawan0 <100456473+AIrawan0@users.noreply.github.com>
2022-04-19 21:08:01 +0700

    Delete Week 3_Asep Irawan_1103190112.sol

--- Week 3_Asep Irawan_1103190112.sol
+++
@@ -1,8 +0,0 @@
-contract p2pkh {
-    Ripemd160 pubKeyHash;
-
-    public function unlock(Sig sig, PubKey pubKey) {
-        require(hash160(pubKey) == this.pubKeyHash);
-        require(checkSig(sig, pubKey));
-    }
-}
